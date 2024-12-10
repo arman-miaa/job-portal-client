e8e8d0ae67a198a359bacbc237e2ce78b7c0f04c
Arman <mdarman365506@gmail.com>
2024-12-10 20:28:02 +0600

    added lottie for animation on register page

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,20 +1,17 @@
+import Lottie from "lottie-react";
 
-
+import registerlottieData from '../../assets/lottie/register.json'
 const Register = () => {
     return (
       <div>
         <div className="hero bg-base-200 min-h-screen">
-          <div className="hero-content flex-col ">
+          <div className="hero-content flex-col lg:flex-row-reverse">
             <div className="text-center ">
-              <h1 className="text-5xl font-bold">Register now!</h1>
-              <p className="py-6">
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                assumenda excepturi exercitationem quasi. In deleniti eaque aut
-                repudiandae et a id nisi.
-              </p>
+            <Lottie animationData={registerlottieData}></Lottie>
             </div>
             <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
               <form className="card-body">
+              <h1 className="text-5xl font-bold">Register now!</h1>
                 <div className="form-control">
                   <label className="label">
                     <span className="label-text">Email</span>
