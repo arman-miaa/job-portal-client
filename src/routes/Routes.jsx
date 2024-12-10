aa5b39423a1d4db6f69fa9a64b68d8e740f89109
Arman <mdarman365506@gmail.com>
2024-12-10 20:12:03 +0600

    added nav and lottie for animation

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,8 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layout/MainLayout";
+import Home from "../pages/Home/Home";
+import Register from "../pages/Register/Register";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+        element: <MainLayout></MainLayout>,
+        children: [
+            {
+                path: '/',
+                element:<Home></Home>,
+            },
+            {
+                path: 'register',
+                element:<Register></Register>,
+            }
+        ]
+        
   },
 ]);
 
